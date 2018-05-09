4359f8f45b603bbf175ae4104dae2d71a825b42a
nekosyakolya <greirex@mail.ru>
2018-05-09 11:30:11 +0300

    перенеены файлы,добавлен вывод информации

--- lw1/Krasilnikova_Olga/lw1/lw1.cpp
+++ lw1/Krasilnikova_Olga/lw1/lw1.cpp
@@ -5,30 +5,56 @@
 #include "CalculateNumberPi.h"
 #include "Point.h"
 
+namespace
+{
+const size_t MIN_WAIT_OBJECTS = 1;
+const int MIN_NUMBER_ARGUMENTS = 2;
+const std::string HELP = "--help";
+} // namespace
+
+void PrintInfo()
+{
+	std::cout << "The program takes two arguments in single-threaded mode:" << std::endl;
+	std::cout << "first argument: \"single\"" << std::endl;
+	std::cout << "second argument: number of iterations" << std::endl;
+
+	std::cout << "The program takes three arguments in multi-threaded mode:" << std::endl;
+	std::cout << "first argument: \"multi\"" << std::endl;
+	std::cout << "second argument: number of threads. Should be from " << MIN_WAIT_OBJECTS << " to " << MAXIMUM_WAIT_OBJECTS << std::endl;
+	std::cout << "third argument: number of iterations" << std::endl;
+}
 int main(int argc, char* argv[])
 {
 	try
 	{
 		unsigned startTime = clock();
-		std::stringstream s;
-		for (size_t i = 1; i != argc; ++i)
+
+		if (argc == MIN_NUMBER_ARGUMENTS && argv[1] == HELP)
 		{
-			s << argv[i] << " ";
+			PrintInfo();
 		}
-		CCalculatorFactory factory;
-		std::unique_ptr<CCalculateNumberPi> calculate = factory.CreateCalculate(s);
-
-		size_t numberIterations;
-		if (!(s >> numberIterations))
+		else
 		{
-			throw std::invalid_argument("Incorrect number of arguments\n");
+			std::stringstream s;
+			for (size_t i = 1; i != argc; ++i)
+			{
+				s << argv[i] << " ";
+			}
+			CCalculatorFactory factory;
+			std::unique_ptr<CCalculateNumberPi> calculate = factory.CreateCalculate(s);
+
+			size_t numberIterations;
+			if (!(s >> numberIterations))
+			{
+				throw std::invalid_argument("Incorrect number of arguments\n");
+			}
+
+			calculate->Calculate(numberIterations);
+			unsigned endTime = clock();
+
+			std::cout << "Pi: " << calculate->GetPi() << std::endl;
+			std::cout << "Time: " << endTime - startTime << std::endl;
 		}
-
-		calculate->Calculate(numberIterations);
-		unsigned endTime = clock();
-
-		std::cout << "Pi: " << calculate->GetPi() << std::endl;
-		std::cout << "Time: " << endTime - startTime << std::endl;
 	}
 	catch (std::exception const& e)
 	{
